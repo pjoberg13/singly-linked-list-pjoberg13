d28b7eb9cb017cd72b77f315ffac049e9b7911bd
Peter <pjoberg13@gmail.com>
2020-12-01 00:37:57 -0500

    finished singly linked list lab

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -3,5 +3,200 @@ package com.zipcodewilmington.singlylinkedlist;
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList<T extends Number>{
+
+    public Node<T> head = null;
+    public Node<T> tail = null;
+
+    //addNode() will add a new node to the list
+    public void addNode(T data) {
+        //Create a new node
+        Node<T> newNode = new Node<T>(data);
+
+        //Checks if the list is empty
+        if(head == null) {
+            //If list is empty, both head and tail will point to new node
+            head = newNode;
+            tail = newNode;
+        }
+        else {
+            //newNode will be added after tail such that tail's next will point to newNode
+            tail.next = newNode;
+            //newNode will become new tail of the list
+            tail = newNode;
+        }
+    }
+
+    //removeNode() will remove a node element from the list
+    public void removeNode(int nodeIndex) {
+        Node<T> current = head;
+        int counter = 0;
+
+        if(head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        if(nodeIndex == 0) {
+            head = head.next;
+        }
+        while(current != null) {
+            if(counter == nodeIndex - 1) {
+                current.next = current.next.next;
+                return;
+            }
+            current = current.next;
+        }
+    }
+
+    //insertAtStart() will add a new element at the head of the list
+    public void insertAtStart(T data) {
+        Node<T> newNode = new Node<T>(data);
+
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public boolean contains(T data) {
+        Node<T> current = head;
+
+        while(current != null) {
+            if(current.data.equals(data)) {
+                return true;
+            } else {
+                current = current.next;
+            }
+        }
+        return false;
+    }
+
+    public Integer findIndexOf(T data) {
+        if(!contains(data)) {
+            return -1;
+        }
+
+        Node<T> current = head;
+        int counter = 0;
+
+        while(current != null) {
+            if(current.data.equals(data)) {
+                return counter;
+            } else {
+                current = current.next;
+                counter++;
+            }
+        }
+        return -1;
+    }
+
+    public T get(int index) {
+        Node<T> current = head;
+        int counter = 0;
+
+        while(current != null) {
+            if(counter == index) {
+                return current.data;
+            }
+            current = current.next;
+            counter++;
+        }
+        return null;
+    }
+
+    public Node<T> getNode(int index) {
+        Node<T> current = head;
+        int counter = 0;
+
+        while(current != null) {
+            if(counter == index) {
+                return current;
+            }
+            current = current.next;
+            counter++;
+        }
+        return null;
+    }
+
+    public Integer getSize() {
+        Node<T> current = head;
+        int sizeCounter = 0;
+
+        if(head == null) {
+            System.out.println("List is empty");
+            return -1;
+        }
+
+        while(current != null) {
+            current = current.next;
+            sizeCounter++;
+        }
+        return sizeCounter;
+    }
+
+    public SinglyLinkedList<T> copyList() {
+        SinglyLinkedList<T> newList = new SinglyLinkedList<T>();
+        Node<T> current = head;
+
+        while(current != null) {
+            newList.addNode(current.data);
+            current = current.next;
+        }
+        return newList;
+    }
+
+    public void reverseList() {
+        //SinglyLinkedList<T> newList = new SinglyLinkedList<T>();
+        Node<T> prev = null;
+        Node<T> current = head;
+        Node<T> next = null;
+
+        while(current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+    }
+
+    public SinglyLinkedList<T> subList(int startIndex, int endIndex) {
+        SinglyLinkedList<T> subList = new SinglyLinkedList<T>();
+        Node<T> current = getNode(startIndex);
+        //int indexCounter = startIndex;
+
+        while(current != getNode(endIndex)) {
+            subList.addNode(current.data);
+            current = current.next;
+        }
+        return subList;
+    }
+
+    //display() will display all the nodes present in the list
+    public void display() {
+        //Node current will point to head
+        Node<T> current = head;
+
+        if(head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        System.out.println("Nodes of singly linked list: ");
+        while(current != null) {
+            //Prints each node by incrementing pointer
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    private static class Node<T> {
+
+        T data;
+        Node<T> next;
+
+        public Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,209 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+
+    @Test
+    public void testListCreation() {
+        SinglyLinkedList<Integer> myList = new SinglyLinkedList<Integer>();
+
+        myList.addNode(2);
+        myList.addNode(3);
+        myList.addNode(5);
+
+        myList.display();
+    }
+
+    @Test
+    public void testInsertAtStart() {
+        SinglyLinkedList<Number> myList = new SinglyLinkedList<Number>();
+        myList.addNode(2);
+        myList.addNode(3f);
+        myList.addNode(5);
+        myList.addNode(7d);
+
+        myList.insertAtStart(1);
+
+        myList.display();
+    }
+
+    @Test
+    public void testContains() {
+        SinglyLinkedList<Integer> myList = new SinglyLinkedList<Integer>();
+
+        myList.addNode(2);
+        myList.addNode(3);
+        myList.addNode(5);
+        myList.addNode(7);
+
+        Assert.assertTrue(myList.contains(7));
+        Assert.assertFalse(myList.contains(21));
+    }
+
+    @Test
+    public void testFindIndexOf() {
+        SinglyLinkedList<Integer> myList = new SinglyLinkedList<Integer>();
+
+        myList.addNode(2);
+        myList.addNode(3);
+        myList.addNode(5);
+        myList.addNode(7);
+
+        int expected = 1;
+        int actual = myList.findIndexOf(3);
+        assertEquals(expected, actual);
+
+        int expectedFalse = -1;
+        int actualFalse = myList.findIndexOf(500);
+        assertEquals(expectedFalse, actualFalse);
+    }
+
+    @Test
+    public void testGetSize1() {
+        SinglyLinkedList<Integer> myList = new SinglyLinkedList<Integer>();
+
+        myList.addNode(2);
+        myList.addNode(3);
+        myList.addNode(5);
+        myList.addNode(7);
+
+        int expected = 4;
+        int actual = myList.getSize();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetSize2() {
+        SinglyLinkedList<Integer> myList = new SinglyLinkedList<Integer>();
+        for(int i = 0; i < 100; i++) {
+            myList.addNode(i);
+        }
+
+        int expected = 100;
+        int actual = myList.getSize();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGet() {
+        SinglyLinkedList<Integer> myList = new SinglyLinkedList<Integer>();
+
+        myList.addNode(2);
+        myList.addNode(3);
+        myList.addNode(5);
+        myList.addNode(7);
+
+        int expected = 3;
+        int actual = myList.get(1);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGet2() {
+        SinglyLinkedList<Integer> myList = new SinglyLinkedList<Integer>();
+
+        myList.addNode(2);
+        myList.addNode(3);
+        myList.addNode(5);
+        myList.addNode(7);
+
+        int expected = 7;
+        int actual = myList.get(3);
+        assertEquals(expected, actual);
+
+        int expectedFalse = 5;
+        int actualFalse = myList.get(2);
+        assertEquals(expectedFalse, actualFalse);
+    }
+
+    @Test
+    public void testCopy() {
+        SinglyLinkedList<Integer> myList = new SinglyLinkedList<Integer>();
+
+        myList.addNode(2);
+        myList.addNode(3);
+        myList.addNode(5);
+        myList.addNode(7);
+
+        SinglyLinkedList<Integer> myNewList = myList.copyList();
+        int expected = myList.getSize();
+        int actual = myNewList.getSize();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testCopy1() {
+        SinglyLinkedList<Integer> myList = new SinglyLinkedList<Integer>();
+
+        myList.addNode(2);
+        myList.addNode(3);
+        myList.addNode(5);
+        myList.addNode(7);
+
+        SinglyLinkedList<Integer> myNewList = myList.copyList();
+        int expected = myList.get(2);
+        int actual = myNewList.get(2);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testNodeRemoval() {
+        SinglyLinkedList<Integer> myList = new SinglyLinkedList<Integer>();
+
+        myList.addNode(2);
+        myList.addNode(3);
+        myList.addNode(5);
+        myList.addNode(7);
+        assertEquals(4, (int) myList.getSize());
+
+        myList.removeNode(1);
+
+        int expected = 3;
+        int actual = myList.getSize();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testListReversal() {
+        SinglyLinkedList<Integer> myList = new SinglyLinkedList<Integer>();
+
+        myList.addNode(2);
+        myList.addNode(3);
+        myList.addNode(5);
+        myList.addNode(7);
+        myList.reverseList();
+
+        int expected = 5;
+        int actual = myList.get(1);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSubList() {
+        SinglyLinkedList<Integer> myList = new SinglyLinkedList<Integer>();
+
+        myList.addNode(2);
+        myList.addNode(3);
+        myList.addNode(5);
+        myList.addNode(7);
+        myList.addNode(11);
+        myList.addNode(13);
+        myList.addNode(17);
+
+        SinglyLinkedList<Integer> subList = myList.subList(2, 6);
+
+        int expected = 5;
+        int actual = subList.get(0);
+        int expected1 = 11;
+        int actual1 = subList.get(2);
+        assertEquals(expected, actual);
+        assertEquals(expected1, actual1);
+    }
 }
